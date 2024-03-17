7c1c7955c0ee2f31ba159b8f7b7fbabfeaa1275c
00014489 <usmonovelbek7@gmail.com>
2024-03-17 21:56:01 +0500

    adding controller

---
+++ CW_MovieApp/CW_MovieApp/Data/MoviesListDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CW_MovieApp.Modules;
+using Microsoft.EntityFrameworkCore;
+
+namespace CW_MovieApp.Data
+{
+    public class MoviesListDbContext : DbContext
+    {
+        public MoviesListDbContext(DbContextOptions<MoviesListDbContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- CW_MovieApp/CW_MovieApp/Modules/Movie.cs
+++ CW_MovieApp/CW_MovieApp/Modules/Movie.cs
@@ -1,4 +1,8 @@
-﻿namespace CW_MovieApp.Modules
+﻿using CW_MovieApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace CW_MovieApp.Modules
 {
     public class Movie
     {
